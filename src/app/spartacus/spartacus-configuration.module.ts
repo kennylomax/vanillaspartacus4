34737ae191a99fdde04097e5c67679c1e46a2130
kennylomax <kennylomax@gmail.com>
2022-06-30 10:11:29 +0200

    first commit

---
+++ src/app/spartacus/spartacus-configuration.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { translationChunksConfig, translations } from "@spartacus/assets";
+import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from "@spartacus/core";
+import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ],
+  providers: [provideConfig(layoutConfig), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig(<OccConfig>{
+    backend: {
+      occ: {
+        baseUrl: 'https://sap-commerce-cloud-2105.ingress.20220627a.klxtrial.shoot.canary.k8s-hana.ondemand.com',
+      }
+    },
+  }), provideConfig(<SiteContextConfig>{
+    context: {
+      currency: ['USD'],
+      language: ['en'],
+    },
+  }), provideConfig(<I18nConfig>{
+    i18n: {
+      resources: translations,
+      chunks: translationChunksConfig,
+      fallbackLang: 'en'
+    },
+  }), provideConfig(<FeaturesConfig>{
+    features: {
+      level: '4.0'
+    }
+  })]
+})
+export class SpartacusConfigurationModule { }
